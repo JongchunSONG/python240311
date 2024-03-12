d933621f3302567b41ce179b641a3beb71a78ab1
JongchunSONG <haruqha@gmail.com>
2024-03-12 13:06:43 +0900

    문자열 테스트

--- DemoStr.py
+++ DemoStr.py
@@ -1,10 +1,19 @@
 # DemoStr.py
 
 #print(dir(str))
-
+ 
 strA = "python is very powerful"
 strB = "파이썬은 매우 강력해"
 
 print(len(strA))
 print(len(strB))
 print(strA.capitalize())
+print("MBC2580".isalnum())
+print("MBC:2580".isalnum())
+data = "<<< spam and ham >>>"
+result = data.strip("<>")
+print(data)
+print(result)
+lst = result.split()
+print(lst)
+print(":)".join(lst))
